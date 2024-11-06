2fbe15c08608af1b99c6e7e2cfec3547118b52dc
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-11-06 22:13:59 +0700

    latihan javascript

--- Ipersonic/script.js
+++ Ipersonic/script.js
@@ -1,1 +1,5 @@
-alert("tes javascript");
+var fullName = "Wisnu Kuncoro Ardianto";
+console.log(fullName);
+
+var footerValue = '<div class="container"><p>© iPersonic Indonesia    <a href="#">Personality Test</a>   <a href="#">Career Test</a>   Persönlichkeitstest   Berufstest   Privacy Policy   Felicitas Heyne   Impressum   Help & Contact   iPersonic supports Audiopedia Foundation  </p><p>English | Deutsch | Español | Français | Português | 中文 | Русский | हिंदी | العربية | 日本語 | Bahasa Indonesia | Italiano | Svenska | Nederlands | עברית | Afrikaans | Bosanski | Català | Čeština | Hrvatski | Magyar | Română | Slovenski | Slovenský | Srpski | Български</p></div>';
+document.querySelector("#footer-container").innerHTML = footerValue;
