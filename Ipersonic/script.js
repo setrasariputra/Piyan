09e77e6a6f92c257f4548bbb69e16645d4ea761f
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-11-11 20:23:19 +0700

    Create module

--- Ipersonic/script.js
+++ Ipersonic/script.js
@@ -1,5 +1,34 @@
-var fullName = "Wisnu Kuncoro Ardianto";
-console.log(fullName);
+class PersonalityQuiz {
+    constructor() {
+        console.log("tes constructor");
+        this.displayFooter();
+    }
 
-var footerValue = '<div class="container"><p>© iPersonic Indonesia    <a href="#">Personality Test</a>   <a href="#">Career Test</a>   Persönlichkeitstest   Berufstest   Privacy Policy   Felicitas Heyne   Impressum   Help & Contact   iPersonic supports Audiopedia Foundation  </p><p>English | Deutsch | Español | Français | Português | 中文 | Русский | हिंदी | العربية | 日本語 | Bahasa Indonesia | Italiano | Svenska | Nederlands | עברית | Afrikaans | Bosanski | Català | Čeština | Hrvatski | Magyar | Română | Slovenski | Slovenský | Srpski | Български</p></div>';
-document.querySelector("#footer-container").innerHTML = footerValue;
+    displayAlert() {
+        alert("running first on module");
+    }
+
+    displayLogo() {
+
+    }
+
+    displayDropdownTopMenu() {
+
+    }
+    
+    displayHeader() {
+
+    }
+
+    displayFooter() {
+        let footerHTML = '<div class="container">';
+        footerHTML += '<p>© iPersonic    Personality Test   Career Test   Persönlichkeitstest   Berufstest   Privacy Policy   Felicitas Heyne   Impressum   Help & Contact   iPersonic supports Audiopedia Foundation</p>';
+        footerHTML += '<p>English | Deutsch | Español | Français | Português | 中文 | Русский | हिंदी | العربية | 日本語 | Bahasa Indonesia | Italiano | Svenska | Nederlands | עברית | Afrikaans | Bosanski | Català | Čeština | Hrvatski | Magyar | Română | Slovenski | Slovenský | Srpski | Български</p>';
+        footerHTML += '</div>';
+        const footerContainer = document.querySelector("#footer-container");
+        footerContainer.innerHTML = footerHTML;
+    }
+}
+
+// initiate Personality Quiz Module
+new PersonalityQuiz();
